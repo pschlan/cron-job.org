4f2875309f00f92ec2de7f1256af432ce5367aa7
Patrick Schlangen <patrick@schlangen.me>
2020-03-26 12:23:51 +0100

    Fix ubuntu build

--- CMake/Modules/FindLibThrift.cmake
+++ CMake/Modules/FindLibThrift.cmake
@@ -3,5 +3,5 @@ find_package(PkgConfig REQUIRED)
 pkg_check_modules(LIBTHRIFT REQUIRED thrift)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(libthrift DEFAULT_MSG LIBTHRIFT_LINK_LIBRARIES LIBTHRIFT_INCLUDE_DIRS)
-mark_as_advanced(LIBTHRIFT_INCLUDE_DIRS LIBTHRIFT_LINK_LIBRARIES)
+find_package_handle_standard_args(libthrift DEFAULT_MSG LIBTHRIFT_LIBRARIES LIBTHRIFT_INCLUDE_DIRS LIBTHRIFT_LIBDIR)
+mark_as_advanced(LIBTHRIFT_INCLUDE_DIRS LIBTHRIFT_LIBRARIES LIBTHRIFT_LIBDIR)
