642f426100db59b244046677ed9eab9325c1eb37
Patrick Schlangen <ps@b1g.de>
2021-08-29 16:45:33 +0200

    API: Add missing newsletter update action

---
+++ api/apimethods/UpdateUserNewsletterSubscribe.php
@@ -0,0 +1,33 @@
+<?php
+require_once('lib/APIMethod.php');
+require_once('resources/User.php');
+
+class UpdateUserNewsletterSubscribe extends AbstractAPIMethod {
+  static function name() {
+    return 'UpdateUserNewsletterSubscribe';
+  }
+
+  public function requiresAuthentication() {
+    return true;
+  }
+
+  public function rateLimits() {
+    return [
+      new RateLimit(1, RateLimit::SECOND)
+    ];
+  }
+
+  public function validateRequest($request) {
+    return (
+         isset($request->subscribe)
+      && in_array($request->subscribe, ['yes', 'no', 'undefined'])
+    );
+  }
+
+  public function execute($request, $sessionToken, $language) {
+    if (!(new UserManager($sessionToken))->updateNewsletterSubscribe($request->subscribe)) {
+      throw new InternalErrorAPIException();
+    }
+    return (object)[];
+  }
+}
