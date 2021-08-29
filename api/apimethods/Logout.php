c1ce4a2b402c9ce66e2ac45c3ac8fb42d766d605
Patrick Schlangen <ps@b1g.de>
2021-08-29 23:48:26 +0200

    Add logout API method

---
+++ api/apimethods/Logout.php
@@ -0,0 +1,28 @@
+<?php
+require_once('lib/APIMethod.php');
+require_once('resources/User.php');
+
+class Logout extends AbstractAPIMethod {
+  static function name() {
+    return 'Logout';
+  }
+
+  public function requiresAuthentication() {
+    return true;
+  }
+
+  public function rateLimits() {
+    return [
+      new RateLimit(1, RateLimit::SECOND)
+    ];
+  }
+
+  public function validateRequest($request) {
+    return true;
+  }
+
+  public function execute($request, $sessionToken, $language) {
+    (new UserManager($sessionToken))->logout();
+    return (object)[];
+  }
+}
