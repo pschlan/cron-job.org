3c80ecbb1abea2912dedd57dc3acce8695116bf4
Patrick Schlangen <ps@b1g.de>
2021-09-05 21:48:10 +0200

    API: Add DeleteAccount API method

---
+++ api/apimethods/DeleteAccount.php
@@ -0,0 +1,41 @@
+<?php
+require_once('lib/APIMethod.php');
+require_once('resources/User.php');
+
+class DeleteAccount extends AbstractAPIMethod {
+  static function name() {
+    return 'DeleteAccount';
+  }
+
+  public function requiresAuthentication() {
+    return true;
+  }
+
+  public function rateLimits() {
+    return [
+      new RateLimit(1, RateLimit::SECOND)
+    ];
+  }
+
+  public function validateRequest($request) {
+    return (
+         isset($request->emailAddress)
+      && isset($request->really)
+      && $request->really === 'reallyDeleteAccount'
+    );
+  }
+
+  public function execute($request, $sessionToken, $language) {
+    try {
+      if (!(new UserManager($sessionToken))->deleteAccount($request->emailAddress)) {
+        throw new InternalErrorAPIException();
+      }
+    } catch (InvalidEmailAddressException $ex) {
+      throw new ForbiddenAPIException();
+    } catch (FailedToDeleteAccountException $ex) {
+      throw new InternalErrorAPIException();
+    }
+
+    return (object)[];
+  }
+}
