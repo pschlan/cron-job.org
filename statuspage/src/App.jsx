929786a784483bb116b8635e57871acbd02cd64d
wuniversales <48658161+wuniversales@users.noreply.github.com>
2021-12-17 20:03:59 +0100

    Added ES lang

--- statuspage/src/App.jsx
+++ statuspage/src/App.jsx
@@ -12,6 +12,7 @@ import { initReactI18next } from 'react-i18next';
 import useLanguageCode from './hooks/useLanguageCode';
 
 import translationEN from './locales/en/translation.json';
+import translationES from './locales/es/translation.json';
 import translationDE from './locales/de/translation.json';
 import translationIT from './locales/it/translation.json';
 
@@ -22,6 +23,9 @@ import { Config } from './utils/Config';
 const LANGUAGE_RESOURCES = {
   en: {
     translation: translationEN
+  },
+  es: {
+    translation: translationES
   },
   de: {
     translation: translationDE
