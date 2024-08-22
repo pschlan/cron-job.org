725b9a6f339ddcaebf841aed04aee644078ae882
Matheus Campos <matheus.rj.ig@gmail.com>
2024-08-21 23:47:54 -0300

    feat: translate statuspage

--- statuspage/src/App.jsx
+++ statuspage/src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import './App.css';
 import { Container, CssBaseline } from '@material-ui/core';
-import { Switch, Route } from 'react-router-dom';
-import StatusPage from './components/StatusPage';
+import React, { useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
 import ErrorPage from './components/ErrorPage';
+import StatusPage from './components/StatusPage';
 
 import i18n from 'i18next';
 import detector from 'i18next-browser-languagedetector';
@@ -11,78 +11,83 @@ import { initReactI18next } from 'react-i18next';
 
 import useLanguageCode from './hooks/useLanguageCode';
 
-import translationEN from './locales/en/translation.json';
 import translationDE from './locales/de/translation.json';
-import translationIT from './locales/it/translation.json';
+import translationEN from './locales/en/translation.json';
 import translationFR from './locales/fr/translation.json';
+import translationIT from './locales/it/translation.json';
+import translationPL from './locales/pl/translation.json';
+import translationPtBR from './locales/pt_BR/translation.json';
+import translationRo from './locales/ro/translation.json';
 import translationRU from './locales/ru/translation.json';
 import translationZhTW from './locales/zh_TW/translation.json';
-import translationRo from './locales/ro/translation.json';
-import translationPL from './locales/pl/translation.json';
 
+import moment from 'moment';
 import 'moment/locale/de';
 import 'moment/locale/it';
-import 'moment/locale/ru';
 import 'moment/locale/pl';
-import moment from 'moment';
+import 'moment/locale/pt-br';
+import 'moment/locale/ru';
 import { Config } from './utils/Config';
 
 const LANGUAGE_RESOURCES = {
-  en: {
-    translation: translationEN
-  },
-  de: {
-    translation: translationDE
-  },
-  it: {
-    translation: translationIT
-  },
-  fr: {
-    translation: translationFR
-  },
-  ru:{
-    translation: translationRU
-  },
-  zh_TW:{
-    translation: translationZhTW
-  },
-  ro:{
-    translation: translationRo
-  },
-  pl:{
-    translation: translationPL
-  }
+    en: {
+        translation: translationEN
+    },
+    de: {
+        translation: translationDE
+    },
+    it: {
+        translation: translationIT
+    },
+    fr: {
+        translation: translationFR
+    },
+    ru: {
+        translation: translationRU
+    },
+    zh_TW: {
+        translation: translationZhTW
+    },
+    ro: {
+        translation: translationRo
+    },
+    pl: {
+        translation: translationPL
+    },
+    pt_BR: {
+        translation: translationPtBR
+    }
 };
 
 i18n
-  .use(detector)
-  .use(initReactI18next)
-  .init({
-    resources: LANGUAGE_RESOURCES,
-    fallbackLng: Config.fallbackLanguage,
-    interpolation: {
-      escapeValue: false
-    }
-  });
+    .use(detector)
+    .use(initReactI18next)
+    .init({
+        resources: LANGUAGE_RESOURCES,
+        fallbackLng: Config.fallbackLanguage,
+        interpolation: {
+            escapeValue: false
+        }
+    });
 
 function App() {
-  const languageCode = useLanguageCode();
+    const languageCode = useLanguageCode();
 
-  useEffect(() => {
-    moment.locale(languageCode);
-  }, [languageCode]);
+    useEffect(() => {
+        moment.locale(languageCode);
+    }, [languageCode]);
 
-  return <>
-    <CssBaseline />
-    <Container maxWidth='md'>
-      <Switch>
-        <Route path="/" exact component={StatusPage} />
-        <Route>
-          <ErrorPage is404={true} />
-        </Route>
-      </Switch>
-    </Container>
-  </>;
+    return <>
+        <CssBaseline />
+        <Container maxWidth='md'>
+            <Switch>
+                <Route path="/" exact component={StatusPage} />
+                <Route>
+                    <ErrorPage is404={true} />
+                </Route>
+            </Switch>
+        </Container>
+    </>;
 }
 
 export default App;
--- statuspage/src/utils/Config.default.js
+++ statuspage/src/utils/Config.default.js
@@ -1,31 +1,32 @@
 export const Config = {
-  //! @note Adjust project name, domains and URLs accordingly.
-  'productName': 'cron-job.org OSS',
-  'baseURL': 'https://example.com',
+    //! @note Adjust project name, domains and URLs accordingly.
+    'productName': 'cron-job.org OSS',
+    'baseURL': 'https://example.com',
 
-  //! @note The URL of the API webserver (/api/ folder in this repo)
-  'apiURL': 'https://api.example.com/',
+    //! @note The URL of the API webserver (/api/ folder in this repo)
+    'apiURL': 'https://api.example.com/',
 
-  //! @note Domain to use whe opening the status page from localhost (e.g. dev environment)
-  'devDomain': 'status.example.com',
+    //! @note Domain to use whe opening the status page from localhost (e.g. dev environment)
+    'devDomain': 'status.example.com',
 
-  /****************************************************************************************
-   ******** The following settings might be kept as is. Customization is optional. ********
-   ****************************************************************************************/
+    /****************************************************************************************
+     ******** The following settings might be kept as is. Customization is optional. ********
+     ****************************************************************************************/
 
-  //! @note List of supported languages. When adding new language file, be sure
-  //!       to add the new language code here.
-  'languages': {
-    'en': 'English',
-    'de': 'Deutsch',
-    'it': 'Italiano',
-    'fr': 'Français',
-    'ru': 'Русский',
-    'zh_TW': '正體中文',
-    'ro': 'Română',
-    'pl': 'Polski'
-  },
+    //! @note List of supported languages. When adding new language file, be sure
+    //!       to add the new language code here.
+    'languages': {
+        'en': 'English',
+        'de': 'Deutsch',
+        'it': 'Italiano',
+        'fr': 'Français',
+        'ru': 'Русский',
+        'zh_TW': '正體中文',
+        'ro': 'Română',
+        'pl': 'Polski',
+        'pt_BR': 'Português Brasil'
+    },
 
-  //! @note Fallback language to use when the user's auto-determined language is not supported.
-  'fallbackLanguage': 'en',
+    //! @note Fallback language to use when the user's auto-determined language is not supported.
+    'fallbackLanguage': 'en',
 };
