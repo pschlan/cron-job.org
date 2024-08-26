d41351842c82ec6be4a050d9e5a02616d5bd569a
Matheus Campos <external.matheus.campos@trisor.de>
2024-08-26 11:43:54 -0300

    fix: identation and rever organize file imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,252 +1,252 @@
-import { CssBaseline } from '@material-ui/core';
-import BarChartIcon from '@material-ui/icons/BarChart';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import FolderIcon from '@material-ui/icons/FolderOutlined';
-import StatusPagesIcon from '@material-ui/icons/NetworkCheck';
-import ScheduleIcon from '@material-ui/icons/Schedule';
-import SettingsIcon from '@material-ui/icons/Settings';
+import React, { useEffect } from 'react';
+import Authenticator from './components/auth/Authenticator';
 import i18n from 'i18next';
 import detector from 'i18next-browser-languagedetector';
-import { SnackbarProvider, useSnackbar } from 'notistack';
-import React, { useEffect } from 'react';
 import { initReactI18next, useTranslation } from 'react-i18next';
-import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
-import Authenticator from './components/auth/Authenticator';
+import { Redirect, Switch, Route, useLocation } from 'react-router-dom';
+import { SnackbarProvider, useSnackbar } from 'notistack';
+import { CssBaseline } from '@material-ui/core';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import ScheduleIcon from '@material-ui/icons/Schedule';
+import SettingsIcon from '@material-ui/icons/Settings';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import FolderIcon from '@material-ui/icons/FolderOutlined';
+import StatusPagesIcon from '@material-ui/icons/NetworkCheck';
 
-import translationDE from './locales/de/translation.json';
 import translationEN from './locales/en/translation.json';
-import translationFR from './locales/fr/translation.json';
+import translationDE from './locales/de/translation.json';
 import translationIT from './locales/it/translation.json';
+import translationRU from './locales/ru/translation.json';
+import translationFR from './locales/fr/translation.json';
+import translationZhTW from './locales/zh_TW/translation.json';
+import translationRo from './locales/ro/translation.json';
 import translationPL from './locales/pl/translation.json';
 import translationPtBR from './locales/pt_BR/translation.json';
-import translationRo from './locales/ro/translation.json';
-import translationRU from './locales/ru/translation.json';
-import translationZhTW from './locales/zh_TW/translation.json';
 
-import AppLayout from './components/AppLayout';
 import AppMenu from './components/AppMenu';
-import AppToolbar from './components/AppToolbar';
-import ConfirmEmailChange from './components/auth/ConfirmEmailChange';
 import Dashboard from './components/dashboard/Dashboard';
-import History from './components/jobs/History';
-import JobEditor from './components/jobs/JobEditor';
 import Jobs from './components/jobs/Jobs';
+import JobEditor from './components/jobs/JobEditor';
+import History from './components/jobs/History';
 import NotFound from './components/misc/NotFound';
 import Settings from './components/settings/Settings';
+import ConfirmEmailChange from './components/auth/ConfirmEmailChange';
+import AppToolbar from './components/AppToolbar';
 import Statistics from './components/statistics/Statistics';
+import AppLayout from './components/AppLayout';
 
 import { updateUserLanguage } from './utils/API';
 
-import moment from 'moment';
 import 'moment/locale/de';
-import 'moment/locale/fr';
 import 'moment/locale/it';
-import 'moment/locale/pl';
-import 'moment/locale/pt-br';
-import 'moment/locale/ro';
 import 'moment/locale/ru';
+import 'moment/locale/fr';
+import 'moment/locale/ro';
+import 'moment/locale/pl';
 import 'moment/locale/zh-tw';
-import { useDispatch, useSelector } from 'react-redux';
-import Folders from './components/jobs/Folders';
-import StatusPageEditor from './components/statuspages/StatusPageEditor';
-import StatusPages from './components/statuspages/StatusPages';
-import useFolders from './hooks/useFolders';
+import 'moment/locale/pt-br';
+import moment from 'moment';
 import useLanguageCode, { getLanguageCode } from './hooks/useLanguageCode';
-import useViewport from './hooks/useViewport';
-import { setUiSetting } from './redux/actions';
 import { Config } from './utils/Config';
+import StatusPages from './components/statuspages/StatusPages';
+import StatusPageEditor from './components/statuspages/StatusPageEditor';
+import useViewport from './hooks/useViewport';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUiSetting } from './redux/actions';
+import useFolders from './hooks/useFolders';
+import Folders from './components/jobs/Folders';
 
 export let snackbarRef = null;
 
 const LANGUAGE_RESOURCES = {
-    en: {
-        translation: translationEN
-    },
-    de: {
-        translation: translationDE
-    },
-    it: {
-        translation: translationIT
-    },
-    ru: {
-        translation: translationRU
-    },
-    fr: {
-        translation: translationFR
-    },
-    zh_TW: {
-        translation: translationZhTW,
-    },
-    ro: {
-        translation: translationRo,
-    },
-    pl: {
-        translation: translationPL,
-    },
-    pt_BR: {
-        translation: translationPtBR,
-    },
+  en: {
+    translation: translationEN
+  },
+  de: {
+    translation: translationDE
+  },
+  it: {
+    translation: translationIT
+  },
+  ru: {
+    translation: translationRU
+  },
+  fr: {
+    translation: translationFR
+  },
+  zh_TW: {
+    translation: translationZhTW,
+  },
+  ro: {
+    translation: translationRo,
+  },
+  pl: {
+    translation: translationPL,
+  },
+  pt_BR: {
+    translation: translationPtBR,
+  },
 };
 
 i18n
-    .use(detector)
-    .use(initReactI18next)
-    .init({
-        resources: LANGUAGE_RESOURCES,
-        fallbackLng: Config.fallbackLanguage,
-        interpolation: {
-            escapeValue: false
-        }
-    });
+  .use(detector)
+  .use(initReactI18next)
+  .init({
+    resources: LANGUAGE_RESOURCES,
+    fallbackLng: Config.fallbackLanguage,
+    interpolation: {
+      escapeValue: false
+    }
+  });
 
 i18n
-    .on('languageChanged', () => {
-        const lang = getLanguageCode(i18n.languages);
-        updateUserLanguage(lang);
-    });
+  .on('languageChanged', () => {
+    const lang = getLanguageCode(i18n.languages);
+    updateUserLanguage(lang);
+  });
 
 function SnackbarReferenceProvider() {
-    snackbarRef = useSnackbar();
-    return <></>;
+  snackbarRef = useSnackbar();
+  return <></>;
 }
 
 function diffLang(lang1, lang2, prefix = '') {
-    for (let key in lang1) {
-        if (typeof (lang2[key]) === 'undefined') {
-            console.log(prefix + key);
-        } else if (typeof (lang1[key]) === 'object') {
-            diffLang(lang1[key], lang2[key], key + '.');
-        }
+  for (let key in lang1) {
+    if (typeof (lang2[key]) === 'undefined') {
+      console.log(prefix + key);
+    } else if (typeof (lang1[key]) === 'object') {
+      diffLang(lang1[key], lang2[key], key + '.');
     }
+  }
 }
 
 function ConsoleMenu({ selectedId, onListItemClick, indentSubItems = false }) {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const folders = useFolders();
+  const folders = useFolders();
 
-    const menuItems = [
+  const menuItems = [
+    {
+      items: [
         {
-            items: [
-                {
-                    id: 'dashboard',
-                    text: t('common.dashboard'),
-                    icon: <DashboardIcon />,
-                    href: '/dashboard'
-                },
-                {
-                    id: 'jobs',
-                    text: t('common.cronjobs'),
-                    icon: <ScheduleIcon />,
-                    href: '/jobs',
-                    subItems: folders.map(folder => (
-                        {
-                            id: 'folders/' + folder.folderId,
-                            text: folder.title,
-                            icon: <FolderIcon />,
-                            href: '/jobs/folders/' + folder.folderId
-                        }))
-                },
-                Config.enableStatusPages && {
-                    id: 'statuspages',
-                    text: t('common.statuspages'),
-                    icon: <StatusPagesIcon />,
-                    href: '/statuspages'
-                },
-                {
-                    id: 'statistics',
-                    text: t('common.statistics'),
-                    icon: <BarChartIcon />,
-                    href: '/statistics'
-                }
-            ]
+          id: 'dashboard',
+          text: t('common.dashboard'),
+          icon: <DashboardIcon />,
+          href: '/dashboard'
         },
         {
-            items: [
-                {
-                    id: 'settings',
-                    text: t('common.settings'),
-                    icon: <SettingsIcon />,
-                    href: '/settings'
-                }
-            ]
+          id: 'jobs',
+          text: t('common.cronjobs'),
+          icon: <ScheduleIcon />,
+          href: '/jobs',
+          subItems: folders.map(folder => (
+            {
+              id: 'folders/' + folder.folderId,
+              text: folder.title,
+              icon: <FolderIcon />,
+              href: '/jobs/folders/' + folder.folderId
+            }))
+        },
+        Config.enableStatusPages && {
+          id: 'statuspages',
+          text: t('common.statuspages'),
+          icon: <StatusPagesIcon />,
+          href: '/statuspages'
+        },
+        {
+          id: 'statistics',
+          text: t('common.statistics'),
+          icon: <BarChartIcon />,
+          href: '/statistics'
         }
-    ];
+      ]
+    },
+    {
+      items: [
+        {
+          id: 'settings',
+          text: t('common.settings'),
+          icon: <SettingsIcon />,
+          href: '/settings'
+        }
+      ]
+    }
+  ];
 
-    return <AppMenu
-        items={menuItems}
-        selectedId={selectedId}
-        onListItemClick={onListItemClick}
-        indentSubItems={indentSubItems}
-    />;
+  return <AppMenu
+    items={menuItems}
+    selectedId={selectedId}
+    onListItemClick={onListItemClick}
+    indentSubItems={indentSubItems}
+  />;
 }
 
 function getSelectedId(loc) {
-    const locParts = loc.split('/');
-    const item = locParts[1];
+  const locParts = loc.split('/');
+  const item = locParts[1];
 
-    if (item === 'jobs') {
-        if (locParts[2] === 'folders' && locParts.length > 3) {
-            return 'folders/' + locParts[3];
-        }
+  if (item === 'jobs') {
+    if (locParts[2] === 'folders' && locParts.length > 3) {
+      return 'folders/' + locParts[3];
     }
+  }
 
-    return item;
+  return item;
 }
 
 function App() {
-    const location = useLocation();
-    const { t } = useTranslation();
-    const { isMobile } = useViewport();
-    const dispatch = useDispatch();
+  const location = useLocation();
+  const { t } = useTranslation();
+  const { isMobile } = useViewport();
+  const dispatch = useDispatch();
 
-    const indentSubItems = !useSelector(state => state.ui && state.ui.menuClosed);
+  const indentSubItems = !useSelector(state => state.ui && state.ui.menuClosed);
 
-    const languageCode = useLanguageCode();
+  const languageCode = useLanguageCode();
 
-    diffLang(translationEN, translationDE);
+  diffLang(translationEN, translationDE);
 
-    useEffect(() => {
-        moment.updateLocale(languageCode, {
-            calendar: LANGUAGE_RESOURCES[languageCode].translation.common.calendarFormat
-        });
-    }, [languageCode]);
+  useEffect(() => {
+    moment.updateLocale(languageCode, {
+      calendar: LANGUAGE_RESOURCES[languageCode].translation.common.calendarFormat
+    });
+  }, [languageCode]);
 
-    return (
-        <SnackbarProvider maxSnack={5} autoHideDuration={5000} preventDuplicate={true}>
-            <SnackbarReferenceProvider />
-            <CssBaseline />
-            <Authenticator>
-                <AppLayout
-                    menuText={t('common.menu')}
-                    menu={<ConsoleMenu indentSubItems={indentSubItems} selectedId={getSelectedId(location.pathname)} onListItemClick={isMobile ? () => dispatch(setUiSetting('menuClosed', true)) : () => null} />}
-                    toolbar={<AppToolbar />}
-                >
-                    <Switch>
-                        <Route path="/dashboard" exact component={Dashboard} />
-                        <Route path="/jobs/folders" exact component={Folders} />
-                        <Route path="/jobs/:jobId/history" exact component={History} />
-                        <Route path="/jobs/create" exact component={JobEditor} />
-                        <Route path="/jobs/:jobId" exact component={JobEditor} />
-                        <Route path="/jobs" exact component={Jobs} />
-                        <Route path="/jobs/folders/:folderId" exact component={Jobs} />
-                        <Route path="/jobs/folders/:folderId/:jobId/history" exact component={History} />
-                        <Route path="/jobs/folders/:folderId/create" exact component={JobEditor} />
-                        <Route path="/jobs/folders/:folderId/:jobId" exact component={JobEditor} />
-                        {Config.enableStatusPages && <Route path="/statuspages/:statusPageId" exact component={StatusPageEditor} />}
-                        {Config.enableStatusPages && <Route path="/statuspages" exact component={StatusPages} />}
-                        <Route path="/settings" exact component={Settings} />
-                        <Route path="/statistics" exact component={Statistics} />
-                        <Route path="/confirmEmailChange/:token" exact component={ConfirmEmailChange} />
-                        <Redirect from="/login" exact to="/dashboard" />
-                        <Redirect from="/signup" exact to="/dashboard" />
-                        <Redirect from="/" exact to="/dashboard" />
-                        <NotFound />
-                    </Switch>
-                </AppLayout>
-            </Authenticator>
-        </SnackbarProvider>
-    );
+  return (
+    <SnackbarProvider maxSnack={5} autoHideDuration={5000} preventDuplicate={true}>
+      <SnackbarReferenceProvider />
+      <CssBaseline />
+      <Authenticator>
+        <AppLayout
+          menuText={t('common.menu')}
+          menu={<ConsoleMenu indentSubItems={indentSubItems} selectedId={getSelectedId(location.pathname)} onListItemClick={isMobile ? () => dispatch(setUiSetting('menuClosed', true)) : () => null} />}
+          toolbar={<AppToolbar />}
+        >
+          <Switch>
+            <Route path="/dashboard" exact component={Dashboard} />
+            <Route path="/jobs/folders" exact component={Folders} />
+            <Route path="/jobs/:jobId/history" exact component={History} />
+            <Route path="/jobs/create" exact component={JobEditor} />
+            <Route path="/jobs/:jobId" exact component={JobEditor} />
+            <Route path="/jobs" exact component={Jobs} />
+            <Route path="/jobs/folders/:folderId" exact component={Jobs} />
+            <Route path="/jobs/folders/:folderId/:jobId/history" exact component={History} />
+            <Route path="/jobs/folders/:folderId/create" exact component={JobEditor} />
+            <Route path="/jobs/folders/:folderId/:jobId" exact component={JobEditor} />
+            {Config.enableStatusPages && <Route path="/statuspages/:statusPageId" exact component={StatusPageEditor} />}
+            {Config.enableStatusPages && <Route path="/statuspages" exact component={StatusPages} />}
+            <Route path="/settings" exact component={Settings} />
+            <Route path="/statistics" exact component={Statistics} />
+            <Route path="/confirmEmailChange/:token" exact component={ConfirmEmailChange} />
+            <Redirect from="/login" exact to="/dashboard" />
+            <Redirect from="/signup" exact to="/dashboard" />
+            <Redirect from="/" exact to="/dashboard" />
+            <NotFound />
+          </Switch>
+        </AppLayout>
+      </Authenticator>
+    </SnackbarProvider>
+  );
 }
 
 export default App;
--- frontend/src/utils/Config.default.js
+++ frontend/src/utils/Config.default.js
@@ -10,58 +10,58 @@ export const Config = {
   'apiDocsURL': 'https://example.com/docs/rest-api.html',
   'variablesDocsURL': 'https://example.com/creating-cron-jobs.html#cron-job-variables',
 
-    //! @note The URL of the API webserver (/api/ folder in this repo)
-    'apiURL': 'https://api.example.com/',
+  //! @note The URL of the API webserver (/api/ folder in this repo)
+  'apiURL': 'https://api.example.com/',
 
-    //! @note Put your reCAPTCHA site key here.
-    'recaptchaSiteKey': 'PLACE_RECAPTCHA_SITE_KEY_HERE',
+  //! @note Put your reCAPTCHA site key here.
+  'recaptchaSiteKey': 'PLACE_RECAPTCHA_SITE_KEY_HERE',
 
-    /****************************************************************************************
-     ******** The following settings might be kept as is. Customization is optional. ********
-     ****************************************************************************************/
+  /****************************************************************************************
+   ******** The following settings might be kept as is. Customization is optional. ********
+   ****************************************************************************************/
 
-    //! @note List of supported languages. When adding new language file, be sure
-    //!       to add the new language code here.
-    'languages': {
-        'en': 'English',
-        'de': 'Deutsch',
-        'it': 'Italiano',
-        'fr': 'Français',
-        'zh_TW': '正體中文',
-        'ro': 'Română',
-        'pl': 'Polski',
-        'pt_BR': 'Português Brasil'
-    },
+  //! @note List of supported languages. When adding new language file, be sure
+  //!       to add the new language code here.
+  'languages': {
+    'en': 'English',
+    'de': 'Deutsch',
+    'it': 'Italiano',
+    'fr': 'Français',
+    'zh_TW': '正體中文',
+    'ro': 'Română',
+    'pl': 'Polski',
+    'pt_BR': 'Português Brasil'
+  },
 
-    //! @note Fallback language to use when the user's auto-determined language is not supported.
-    'fallbackLanguage': 'en',
+  //! @note Fallback language to use when the user's auto-determined language is not supported.
+  'fallbackLanguage': 'en',
 
-    //! @note Session token refresh interval in ms.
-    'sessionRefreshInterval': 10 * 60 * 1000,
+  //! @note Session token refresh interval in ms.
+  'sessionRefreshInterval': 10 * 60 * 1000,
 
-    //! @note A donation box can be enabled here. It will be shown when the user exceeds the
-    //!       configured successful job count threshold.
-    'donationBox': {
-        'enable': false,
-        'successfulJobsThreshold': 1,
-        'hostedButtonId': 'PLACE_PAYPAL_HOSTED_BUTTON_ID_HERE'
-    },
+  //! @note A donation box can be enabled here. It will be shown when the user exceeds the
+  //!       configured successful job count threshold.
+  'donationBox': {
+    'enable': false,
+    'successfulJobsThreshold': 1,
+    'hostedButtonId': 'PLACE_PAYPAL_HOSTED_BUTTON_ID_HERE'
+  },
 
-    //! @note If you want to offer a membership, define the amounts here.
-    'sustainingMembership': {
-        'enable': false,
-        'amountsAMonth': [5, 10, 20],
-        'amountsAYear': [12, 24, 36],
-        'box': {
-            'enable': false,
-            'successfulJobsThreshold': 1
-        }
-    },
+  //! @note If you want to offer a membership, define the amounts here.
+  'sustainingMembership': {
+    'enable': false,
+    'amountsAMonth': [5, 10, 20],
+    'amountsAYear': [12, 24, 36],
+    'box': {
+      'enable': false,
+      'successfulJobsThreshold': 1
+    }
+  },
 
-    //! @note Status pages support can be optionally enabled here.
-    'enableStatusPages': false,
-    'statusPageDomain': 'status.example.com',
-    'maxLogoSize': 1024 * 128,
+  //! @note Status pages support can be optionally enabled here.
+  'enableStatusPages': false,
+  'statusPageDomain': 'status.example.com',
+  'maxLogoSize': 1024 * 128,
 
-    'enableYubicoOTP': true,
+  'enableYubicoOTP': true,
 };
