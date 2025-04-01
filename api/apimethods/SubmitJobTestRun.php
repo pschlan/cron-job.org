04ff6cfba1687fbb2b1e696f6af1e84c7623917b
Patrick Schlangen <ps@b1g.de>
2025-04-01 14:35:54 +0200

    api: Respect job URL deny list in SubmitJobTestRun

--- api/apimethods/SubmitJobTestRun.php
+++ api/apimethods/SubmitJobTestRun.php
@@ -1,4 +1,5 @@
 <?php
+require_once('config/denylists.inc.php');
 require_once('lib/APIMethod.php');
 require_once('resources/Job.php');
 require_once('lib/RecaptchaVerifier.php');
@@ -21,6 +22,17 @@ class SubmitJobTestRun extends AbstractAPIMethod {
 
   public function validateRequest($request) {
     global $config;
+    global $jobUrlDenyList;
+
+    if (isset($request->job->url)) {
+      $lowerUrl = strtolower($request->job->url);
+      foreach ($jobUrlDenyList as $deniedUrl) {
+        if (strpos($lowerUrl, $deniedUrl) !== false) {
+          return false;
+        }
+      }
+    }
+
     return (
          isset($request->jobId)
       && is_numeric($request->jobId)
