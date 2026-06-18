9c8c17cba7f2bfb2bff878134683f32389fc366a
Patrick Schlangen <patrick@schlangen.me>
2026-06-18 10:32:46 +0200

    frontend, api: Extend dashboard

--- api/apimethods/GetDashboard.php
+++ api/apimethods/GetDashboard.php
@@ -23,6 +23,19 @@ class GetDashboard extends AbstractAPIMethod {
     return true;
   }
 
+  private static function trimJob($job) {
+    return (object)[
+      'jobId'         => $job->jobId,
+      'folderId'      => $job->folderId,
+      'title'         => $job->title,
+      'url'           => $job->url,
+      'lastStatus'    => $job->lastStatus,
+      'lastDuration'  => $job->lastDuration,
+      'nextExecution' => $job->nextExecution,
+      'sslCertExpiry' => isset($job->sslCertExpiry) ? $job->sslCertExpiry : null
+    ];
+  }
+
   public function execute($request, $sessionToken, $language) {
     $jobs = (new JobManager($sessionToken))->getJobs();
     if (!$jobs) {
@@ -51,12 +64,40 @@ class GetDashboard extends AbstractAPIMethod {
 
     $userProfile = (new UserManager($sessionToken))->getProfile();
 
+    $upcomingJobs = array_filter($jobs->jobs, function($item) {
+      return $item->enabled && isset($item->nextExecution) && $item->nextExecution !== null;
+    });
+    usort($upcomingJobs, function($a, $b) {
+      return $a->nextExecution <=> $b->nextExecution;
+    });
+    $upcomingJobs = array_map([self::class, 'trimJob'], array_slice($upcomingJobs, 0, 5));
+
+    $certExpiryThreshold = time() + 14 * 86400;
+    $expiringCerts = array_filter($jobs->jobs, function($item) use ($certExpiryThreshold) {
+      return isset($item->sslCertExpiry) && $item->sslCertExpiry > 0 && $item->sslCertExpiry <= $certExpiryThreshold;
+    });
+    usort($expiringCerts, function($a, $b) {
+      return $a->sslCertExpiry <=> $b->sslCertExpiry;
+    });
+    $expiringCerts = array_map([self::class, 'trimJob'], $expiringCerts);
+
+    $failedJobList = array_filter($jobs->jobs, function($item) {
+      return $item->lastStatus > 1;
+    });
+    usort($failedJobList, function($a, $b) {
+      return [$b->lastStatus, $a->title] <=> [$a->lastStatus, $b->title];
+    });
+    $failedJobList = array_map([self::class, 'trimJob'], $failedJobList);
+
     $result = (object)[
       'events'              => $events,
       'enabledJobs'         => count(array_filter($jobs->jobs, function($item) { return $item->enabled; })),
       'disabledJobs'        => count(array_filter($jobs->jobs, function($item) { return !$item->enabled; })),
       'successfulJobs'      => count(array_filter($jobs->jobs, function($item) { return $item->lastStatus == 1;})),
       'failedJobs'          => count(array_filter($jobs->jobs, function($item) { return $item->lastStatus > 1; })),
+      'upcomingJobs'        => $upcomingJobs,
+      'expiringCerts'       => $expiringCerts,
+      'failedJobList'       => $failedJobList,
       'newsletterSubscribe' => $userProfile->newsletterSubscribe,
       'notificationsAutoDisabled' => $userProfile->notificationsAutoDisabled
     ];
